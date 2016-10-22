d17215082836ee6a980267fc9ac3e6a4fc6d1c87
wayling <wayling@example.com>
2016-10-22 12:34:41 +0800

    add more constructor for trace

--- before.c
+++ before.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void __attribute__((constructor)) before_main(void)
+static void __attribute__((constructor)) before_main1(void)
 {
-	printf("Before main\n");
+	printf("Before main1\n");
+}
+
+static void __attribute__((constructor)) before_main2(void)
+{
+	printf("Before main2\n");
+}
+
+static void __attribute__((constructor)) before_main3(void)
+{
+	printf("Before main3\n");
 }
 
 int main()
